06b07b17cf0fa9a20d5b882d0ccce1a7205d74e9
JuanitoDeLaTorre <home.john.thomas@gmail.com>
2023-04-18 08:52:06 -0400

    header and routes done

--- script.js
+++ script.js
@@ -1,19 +1,21 @@
 const headerLogo = document.querySelector("#logo");
+const header = document.querySelector("#header");
+const colorBlock = document.querySelector("#colorBlock");
 
 const home = document.querySelector("#home");
-const homeShadow = "drop-shadow( 0px 0px 8px rgb(218, 187, 14)"; //gold
+const homeShadow = "rgb(218, 187, 14)"; //gold
 
 const projects = document.querySelector("#projects");
-const projectsShadow = "drop-shadow( 0px 0px 8px rgb(16, 175, 16)"; //green
+const projectsShadow = "rgb(16, 175, 16)"; //green
 
 const about = document.querySelector("#about");
-const aboutShadow = "drop-shadow( 0px 0px 8px rgb(73, 127, 215)"; //blue
+const aboutShadow = "rgb(73, 127, 215)"; //blue
 
 const contact = document.querySelector("#contact");
-const contactShadow = "drop-shadow( 0px 0px 8px rgb(139, 52, 190)"; //purple
+const contactShadow = "rgb(139, 52, 190)"; //purple
 
 const resume = document.querySelector("#resume");
-const resumeShadow = "drop-shadow( 0px 0px 8px rgba(255, 255, 255, 0.8))"; //white
+const resumeShadow = "rgb(255, 255, 255)"; //white
 
 const navItems = [
   { element: home, shadow: homeShadow },
@@ -23,15 +25,25 @@ const navItems = [
   { element: resume, shadow: resumeShadow },
 ];
 
-console.log(navItems[0].shadow);
+function trimColor(color) {
+  return color.replace("rgb(", "").replace(")", "");
+}
 
 navItems.forEach((item) => {
   item.element.addEventListener("mouseover", () => {
-    headerLogo.style.filter = item.shadow;
+    headerLogo.style.filter = `drop-shadow( 0px 0px 8px ${item.shadow})`;
+
+    colorBlock.style.backgroundColor = `rgba(${trimColor(item.shadow)},0.3)`;
+    colorBlock.style.animation =
+      "2s swoop cubic-bezier(0.39, 0.17, 0.01, 0.98) forwards";
   });
 
   item.element.addEventListener("mouseout", () => {
-    console.log(item.shadow);
-    headerLogo.style.filter = "none";
+    console.log(window.location.href);
+    headerLogo.style.filter = `drop-shadow( 0px 0px 40px rgba(${trimColor(
+      item.shadow
+    )}, 1))`;
+
+    colorBlock.style.animation = "none";
   });
 });
